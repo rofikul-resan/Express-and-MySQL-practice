d32af9030565e757da710ac8056e36e745e558b1
rofikul-resan <resan6231@gmail.com>
2024-10-14 00:54:59 +0600

    serve hetm fille and fix css file linking problem

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
 
 // important variable
 const port = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 // create app in express server
 const app = express();
 app.use(cors());
+
+// middleware
+app.use(express.static(path.join(__dirname, "client")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
