43a76bdd15265e645703a8f90263c22113c359ed
rofikul-resan <resan6231@gmail.com>
2024-10-14 01:40:45 +0600

    mysql data server  connecting problem fix with mysql2 packege

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors";
 import path from "path";
+import env from "dotenv";
+import mysql from "mysql2";
 
 // important variable
 const port = process.env.PORT || 5000;
 const __dirname = path.resolve();
+env.config();
 
 // create app in express server
 const app = express();
@@ -12,6 +15,20 @@ app.use(cors());
 
 // middleware
 app.use(express.static(path.join(__dirname, "client")));
+
+// connect to database
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: process.env.MYSQL_PASS,
+});
+
+try {
+  db.connect();
+  console.log("Connected to database");
+} catch (error) {
+  console.log(error);
+}
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/index.html"));
